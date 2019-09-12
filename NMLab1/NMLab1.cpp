b5a47d6dda0541f1e59f2c821481ff986c205ed4
Runi <runi54rus@gmail.com>
2019-09-12 21:26:32 +0700

    Class System, LU

--- NMLab1/NMLab1.cpp
+++ NMLab1/NMLab1.cpp
@@ -8,66 +8,116 @@
 
 typedef float form;
 
+
+class System
+{
+	Matrix A;
+	Matrix L;
+	Matrix U;
+
+	form *b;
+	form *x;
+	form *y;
+
+	System(Matrix M, form *b):A(M), L(A), U(A)
+	{
+		this->b = b;
+		x = b;
+		y = b;
+	}
+
+	void Decompose()
+	{
+		for (int i = 0; i < A.n; i++)
+		{
+			for (int j = i - A.k; j <= i; j++)
+			{
+				double sumL = 0;
+				double sumU = 0;
+
+				for (int k = i - A.k; k < i; k++)
+				{
+					sumU += L(j, k)*U(k, i);
+				}
+				for (int k = i - A.k; k < j; k++)
+				{
+					sumL += L(i, k)*U(k, j);
+				}
+
+				U(j, i) = A(j, i) - sumU;
+				
+				if (i == j) break;
+				L(i, j) = (A(i, j) - sumL) / U(j, j);
+			}
+		}
+	}
+
+	void findY()
+	{
+		for (int i = 0; i < n; i++)
+		{
+
+		}
+	}
+};
+
 class Matrix
 {
 public:
-	form **A;
+	form **al;
+	form **au;
+	form *di;
+
 	int n;
 	int m;
 	int k;
 
-	Matrix(int n, int m)
-	{
-		this->n = n;
-		this->m = m;
-		k = m / 2;
-		A = new form*[n];
-		for (int i = 0; i < n; i++)
-		{
-
-			A[i] = new form[m];
-			for (int j = 0; j < m; j++)
-			{
-				A[i][j] = 0;
-			}
-		}
-	}
-
-	Matrix(std::ifstream fi, int n, int m)
-	{
-		fi.open("input.txt");
-		A = new form*[n];
-		for (int i = 0; i < n; i++)
-		{
-			this->n = n;
-			this->m = m;
-			k = m / 2;
-			A[i] = new form[m];
-			for (int j = 0; j < m; j++)
-			{
-				fi >> A[i][j];
-			}
-		}
-	}
-
 	Matrix(Matrix *M)
 	{
-		A = M->A;
 		this->k = M->k;
-		this->m = M->m;
 		this->n = M->n;
+		al = M->al;
+		m = M->m;
+		au = M->au;
+		di = M->di;
 	}
 
-	form & operator ()(const int &row, const int &column) const
+	Matrix(std::ifstream fl, std::ifstream fu, std::ifstream fd, int n, int k)
+	{
+		this->k = k;
+		this->n = n;
+		m = 2 * k + 1;
+		al = new form*[n];
+		au = new form*[n];
+		di = new form[n];
+
+		for (int i = 0; i < n; i++)
+		{
+			al[i] = new form[k];
+			au[i] = new form[k];
+
+			for (int j = 0; j < k; j++)
+			{
+				fl >> al[i][j];
+				fu >> au[i][j];
+			}
+			fd >> di[i];
+		}
+	}
+
+
+	form & operator ()(const int &i, const int &j) const
 	{
 		bool contains = false; //проверить
-		if (abs(row - column) > k)
+		if (abs(i-j) > k)
 		{
 			form tmp = 0;
 			return tmp;
 		}
-		int di = k + 1;
-		return A[row][di - row + column];
+
+		if (i == j) return di[i];
+		if (j < i) return al[i][k - (i - j)];
+		return au[j][k - (j - i)];
 	}
 
 	/*form operator ()(const int i, const int j) const
@@ -79,8 +129,6 @@ public:
 
 int main()
 {
-
-	Matrix M(3, 4);
 	M(2, 2) = 4;
 	Matrix L(M);
 	Matrix U(M);
@@ -108,18 +156,7 @@ int main()
 			{
 				sum += L(j, k)*U(k, i);
 			}
-			L(j,i) = (M(j, i) - sum) / U(i, i);
+			L(j, i) = (M(j, i) - sum) / U(i, i);
 		}
 	}
 }
-
-// Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
-// Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
-
-// Советы по началу работы 
-//   1. В окне обозревателя решений можно добавлять файлы и управлять ими.
-//   2. В окне Team Explorer можно подключиться к системе управления версиями.
-//   3. В окне "Выходные данные" можно просматривать выходные данные сборки и другие сообщения.
-//   4. В окне "Список ошибок" можно просматривать ошибки.
-//   5. Последовательно выберите пункты меню "Проект" > "Добавить новый элемент", чтобы создать файлы кода, или "Проект" > "Добавить существующий элемент", чтобы добавить в проект существующие файлы кода.
-//   6. Чтобы снова открыть этот проект позже, выберите пункты меню "Файл" > "Открыть" > "Проект" и выберите SLN-файл.
